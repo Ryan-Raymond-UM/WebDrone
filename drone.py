f1e4bc5660aaab4a08367643bfbc011ecbb75375
Ryan Raymond <ryanray@umich.edu>
2025-11-29 01:44:09 -0600

    Fixed for low-latency video with picamera2

--- drone.py
+++ drone.py
@@ -1,14 +1,31 @@
 from websockets.asyncio.client import connect
 import aiortc
 import aiortc.contrib.media
+from av import VideoFrame
+from fractions import Fraction
+import time
 import asyncio
 import json
+import picamera2
+
+class PiCamera(aiortc.VideoStreamTrack):
+    def __init__(self):
+        super().__init__()
+        self.picam = picamera2.Picamera2()
+        self.picam.start()
+    async def recv(self):
+        frame = self.picam.capture_array()[:, :, :3]
+        video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
+        video_frame.pts = time.time_ns() // 1000
+        video_frame.time_base = Fraction(1, 1_000_000)
+
+        return video_frame
 
 player = None
 data_channel = None
 peer_connection = None
 websocket = None
-video_track = None
+video_track = PiCamera()
 
 async def message_handler(message):
     message = json.loads(message)
@@ -22,6 +39,7 @@ async def offer_handler(offer):
     global peer_connection, websocket, video_track
 
     peer_connection.addTrack(video_track)
+    print(await video_track.recv())
     sessionDescription = aiortc.RTCSessionDescription(offer['sdp'], offer['type'])
     await peer_connection.setRemoteDescription(sessionDescription);
     answer = await peer_connection.createAnswer()
@@ -34,8 +52,9 @@ async def main():
     global peer_connection, websocket, video_track, player
 
     peer_connection = aiortc.RTCPeerConnection()
-    player = aiortc.contrib.media.MediaPlayer('video.mp4')
-    video_track = player.video
+    #player = aiortc.contrib.media.MediaPlayer('udp://localhost:5555')
+    #player = aiortc.contrib.media.MediaPlayer('video.mp4')
+    #video_track = player.video
     #peer_connection.addTrack(video_track) # Commenting this line prevents error
 
     @peer_connection.on('datachannel')
@@ -48,7 +67,7 @@ async def main():
         def onmessage(message):
             print(f'New Message: {message}')
     
-    async with connect('ws://localhost:8765/drone') as websocket:
+    async with connect('ws://CF-54-3-4a96b566:8765/drone') as websocket:
         while True:
             message = await websocket.recv()
             await message_handler(message)
